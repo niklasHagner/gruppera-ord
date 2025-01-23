0ef278580e161f996cbf7e4ce3a5ff2c376ae53b
niklasHagner <niklas.hagner@outlook.com>
2025-01-23 11:05:29 +0100

    Format games in separate file

---
+++ games-new-format.js
@@ -0,0 +1,263 @@
+window.games = [
+  {
+    date: "2025-01-01",
+    difficulty: 1,
+    themes: [
+      { name: "Känslor", words: ["Glad", "Ledsen", "Arg", "Irriterad"] },
+      { name: "Färger", words: ["Röd", "Blå", "Grön", "Gul"] },
+      { name: "Djur", words: ["Hund", "Katt", "Fågel", "Fisk"] },
+      { name: "Frukter", words: ["Äpple", "Banan", "Apelsin", "Druva"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 1,
+    themes: [
+      { name: "Känslor", words: ["Lycklig", "Arg", "Lugn", "Frustrerad"] },
+      { name: "Färger", words: ["Lila", "Turkos", "Magenta", "Beige"] },
+      { name: "Djur", words: ["Häst", "Ko", "Får", "Get"] },
+      { name: "Frukter", words: ["Mango", "Persika", "Ananas", "Jordgubbe"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 1,
+    themes: [
+      { name: "Byggnader", words: ["Hus", "Skyskrapa", "Stuga", "Slott"] },
+      { name: "Media", words: ["Bok", "Film", "Tidning", "Podcast"] },
+      { name: "Jobb", words: ["Läkare", "Ingenjör", "Lärare", "Brandman"] },
+      { name: "Sporter", words: ["Fotboll", "Basket", "Tennis", "Simning"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 1,
+    themes: [
+      { name: "Fordon", words: ["Bil", "Cykel", "Buss", "Tåg"] },
+      { name: "Instrument", words: ["Gitarr", "Piano", "Trummor", "Fiol"] },
+      { name: "Möbler", words: ["Soffa", "Stol", "Bord", "Säng"] },
+      { name: "Kläder", words: ["Tröja", "Byxor", "Jacka", "Skor"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 1,
+    themes: [
+      { name: "Material", words: ["Sten", "Järn", "Stål", "Glas"] },
+      { name: "Kryddor", words: ["Dill", "Persilja", "Gräslök", "Koriander"] },
+      { name: "Rotfrukter", words: ["Potatis", "Kålrot", "Morot", "Rödbeta"] },
+      { name: "Drycker", words: ["Kaffe", "Te", "Juice", "Vatten"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 2,
+    themes: [
+      { name: "Halloween", words: ["Spöke", "Pumpa", "Spindel", "Häxa"] },
+      { name: "Dator", words: ["Mus", "Skärm", "Disk", "Minne"] },
+      { name: "Böcker", words: ["Papper", "Bläck", "Omslag", "Rygg"] },
+      { name: "Hockey", words: ["Klubba", "Puck", "Mål", "Hjälm"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 1,
+    themes: [
+      { name: "Musik", words: ["Grunge", "Rock", "Pop", "Metal"] },
+      { name: "Tidning", words: ["Morgon", "Kväll", "Utgåva", "Nummer"] },
+      { name: "Fotboll", words: ["Offside", "Frispark", "Inkast", "Utspark"] },
+      { name: "Förpackningar", words: ["Låda", "Burk", "Box", "Paket"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 1,
+    themes: [
+      { name: "Väder", words: ["Sol", "Regn", "Snö", "Vind"] },
+      { name: "Rymden", words: ["Stjärna", "Planet", "Kometa", "Asteroid"] },
+      { name: "Mat", words: ["Pizza", "Hamburgare", "Sushi", "Pasta"] },
+      { name: "Blommor", words: ["Ros", "Tulpan", "Lilja", "Orkidé"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 3,
+    themes: [
+      { name: "Kontinenter", words: ["Afrika", "Asien", "Europa", "Australien"] },
+      { name: "Hav", words: ["Atlanten", "Stilla havet", "Indiska oceanen", "Arktiska havet"] },
+      { name: "Språk", words: ["Engelska", "Spanska", "Kinesiska", "Arabiska"] },
+      { name: "Teknologier", words: ["AI", "Blockchain", "IoT", "VR"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 2,
+    themes: [
+      { name: "Språk", words: ["Grekiska", "Spanska", "Ryska", "Mandarin"] },
+      { name: "Mytologi", words: ["Zeus", "Thor", "Anubis", "Odin"] },
+      { name: "Fordon", words: ["Flygplan", "Båt", "Cykel", "Kajak"] },
+      { name: "Instrument", words: ["Flöjt", "Trumpet", "Saxofon", "Harpa"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 3,
+    themes: [
+      { name: "Kaffe", words: ["Termos", "Pulver", "Kapsel", "Brygg"] },
+      { name: "Pålägg", words: ["Ost", "Sallad", "Tomat", "Skinka"] },
+      { name: "Uteleksaker", words: ["Pulka", "Scooter", "Skridsko", "Styltor"] },
+      { name: "Tangenter", words: ["Space", "Control", "Escape", "Tab"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 2,
+    themes: [
+      { name: "Högtider", words: ["Midsommar", "Lucia", "Påsk", "Jul"] },
+      { name: "Varumärken", words: ["IKEA", "ABBA", "Volvo", "Spotify"] },
+      { name: "Natur", words: ["Fjäll", "Gran", "Sjö", "Älg"] },
+      { name: "Tårtor", words: ["Prinsess", "Budapest", "Chocklad", "Rull"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 2,
+    themes: [
+      { name: "Folktro", words: ["Tomte", "Näcken", "Troll", "Gnom"] },
+      { name: "Fika", words: ["Drömmar", "Dammsugare", "Längd", "Bulle"] },
+      { name: "Is", words: ["Vak", "Halka", "Frys", "Kall"] },
+      { name: "Spel", words: ["Kubb", "Fia", "Yatzy", "Vändtia"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 2,
+    themes: [
+      { name: "Djur", words: ["Haj", "Orm", "Lejon", "Skorpion"] },
+      { name: "Morgonrutiner", words: ["Kaffe", "Dusch", "Tandborste", "Tidning"] },
+      { name: "Väderfenomen", words: ["Tornado", "Regnbåge", "Åska", "Dimma"] },
+      { name: "Magiföremål", words: ["Stav", "Spegel", "Amulett", "Kittel"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 2,
+    themes: [
+      { name: "Parfymdofter", words: ["Mysk", "Vanilj", "Lavendel", "Ros"] },
+      { name: "Verktygslådan", words: ["Hammare", "Skruvmejsel", "Såg", "Tång"] },
+      { name: "Fartygstyper", words: ["U-båt", "Segelbåt", "Färja", "Jakt"] },
+      { name: "Konstnärligt", words: ["Pensel", "Grafit", "Palett", "Staffli"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 3,
+    themes: [
+      { name: "Osynligheter", words: ["Kärlek", "Luft", "Tid", "Gravitation"] },
+      { name: "Ljud", words: ["Knarr", "Pip", "Brus", "Muller"] },
+      { name: "Yrken", words: ["Kusk", "Smed", "Bagare", "Fiskare"] },
+      { name: "Färgglatt", words: ["Regnbåge", "Papegoja", "Blomma", "Konfetti"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 3,
+    themes: [
+      { name: "Aggregationsformer", words: ["Fast", "Flytande", "Gas", "Plasma"] },
+      { name: "Stearinljustyper", words: ["Värme", "Kron", "Block", "Doft"] },
+      { name: "Ordbehandling", words: ["Kopiera", "Klipp", "Klistra", "Radera"] },
+      { name: "Spel", words: ["Domino", "Schack", "Kalaha", "Jenga"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 3,
+    themes: [
+      { name: "Filmyrken", words: ["Producent", "Regissör", "Skådis", "Fotograf"] },
+      { name: "Adjektiv", words: ["Minst", "Flera", "Större", "Gammal"] },
+      { name: "Verb", words: ["Peta", "Röra", "Kastade", "Vänd"] },
+      { name: "Kött", words: ["Fågel", "Nöt", "Fläsk", "Vilt"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 4,
+    themes: [
+      { name: "Byggnader på gården", words: ["Lada", "Loge", "Magasin", "Silo"] },
+      { name: "ABBA", words: ["Björn", "Avatar", "SOS", "Brighton"] },
+      { name: "På åkern", words: ["Korn", "Majs", "Rova", "Beta"] },
+      { name: "Alice Tegnérs visor", words: ["Olle", "Ekorre", "Blåsippa", "Videung"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 3,
+    themes: [
+      { name: "På huvudet", words: ["Tupé", "Diadem", "Papiljott", "Kräfthatt"] },
+      { name: "Orter döpta efter personer", words: ["Dorotea", "Fredrika", "Vilhelmina", "Karlstad"] },
+      { name: "På valdagen", words: ["Skärm", "Kuvert", "Legitimation", "Förrättare"] },
+      { name: "Oops!", words: ["Råka", "Sjabbla", "Missa", "Fumla"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 4,
+    themes: [
+      { name: "Låneord från franska", words: ["Byrå", "Enorm", "Populär", "Maskerad"] },
+      { name: "Samma fem bokstäver", words: ["Paris", "Prisa", "Rispa", "Spira"] },
+      { name: "Starkt i glaset", words: ["Geting", "Nubbe", "Rackabajsare", "Järn"] },
+      { name: "Fantasidjur", words: ["Grip", "Skvader", "Drake", "Basilisk"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 3,
+    themes: [
+      { name: "Stockholms blodbad", words: ["Gamla stan", "Tyrann", "Vasa", "Danmark"] },
+      { name: "Första ordet i dokusåpa", words: ["Paradise", "Big Brother", "Expedition", "Robinson"] },
+      { name: "På dåligt humör", words: ["Grinig", "Sur", "Nere", "Missnöjd"] },
+      { name: "På fotbollsplanen", words: ["Stolpe", "Linje", "Flagga", "Mittpunkt"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 4,
+    themes: [
+      { name: "Pastasorter", words: ["Snäcka", "Fjäril", "Rör", "Skruv"] },
+      { name: "Palindrom", words: ["Kajak", "Aja baja", "DVD", "Kivik"] },
+      { name: "Börjar på jul-", words: ["Afton", "Klapp", "Bord", "Gran"] },
+      { name: "Tidningar", words: ["Filter", "Land", "Vi", "Bamse"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 3,
+    themes: [
+      { name: "Att låsa upp teknik med", words: ["Lösenord", "Ansikte", "Pinkod", "Fingeravtryck"] },
+      { name: "Namn från naturen", words: ["Sten", "Bo", "Löv", "Bäck"] },
+      { name: "Filmer med hund", words: ["Lassie", "Scooby-Doo", "101 dalmatiner", "Saltkråkan"] },
+      { name: "Överplagg", words: ["Duffel", "Kavaj", "Morgonrock", "Anorak"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 3,
+    themes: [
+      { name: "Nobelpriset", words: ["Fysik", "Medicin", "Fred", "Kemi"] },
+      { name: "Danser", words: ["Bolero", "Samba", "Vals", "Polska"] },
+      { name: "Fåglar", words: ["Tärna", "Mes", "Råka", "Trut"] },
+      { name: "Programspråk", words: ["C", "R", "Pascal", "Python"] }
+    ]
+  },
+  {
+    date: "2025-01-01",
+    difficulty: 4,
+    themes: [
+      { name: "Tatueringar", words: ["Hjärta", "Dödskalle", "Bläck", "Nål"] },
+      { name: "Blir platser som slutar på -holm", words: ["Stock", "Vax", "Drottning", "Box"] },
+      { name: "Synonymer till Bar", words: ["Naken", "Kal", "Ölservering", "Kånkade"] },
+      { name: "I Östersjön", words: ["Åland", "Strömming", "Säl", "Tång"] }
+    ]
+  }
+];
+
